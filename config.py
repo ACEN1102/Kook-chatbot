e9086e3012826e2922b2240d1e380b7b36bfdefa
Acen <liuhyaw@digitalchina.com>
2025-03-19 15:06:21 +0800

    隐私处理

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # KOOK机器人配置
-BOT_TOKEN = "1/MTkxNTY=/t8eHtHi6XIkI5X79rP7H0w=="
+BOT_TOKEN = ""
 GATEWAY_URL = "https://www.kookapp.cn/api/v3/gateway/index"
 
 # 全局变量
 BOT_USER_ID = "961695898"  # 机器人的用户ID
 
 # 大模型配置
-DEEPSEEK_API_KEY = "sk-addfdeaef8d84d47b4d1b39119fd8125"
+DEEPSEEK_API_KEY = ""
 DEEPSEEK_BASE_URL = "https://api.deepseek.com"
 SYSTEM_PROMPT = "你是一个KOOK聊天室助手，当你觉得用户的提问难以回答时，可以借助工具调用。"
 
